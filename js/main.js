d2f8d8afcc1dc47aa9cba3dbd94316fe409c99b4
AllGoSite <66184790+AllGoSite@users.noreply.github.com>
2020-06-01 15:55:41 +0100

    window

--- js/main.js
+++ js/main.js
@@ -152,20 +152,22 @@ jQuery(document).ready(function( $ ) {
 
   // Clients carousel (uses the Owl Carousel library)
 
-  $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
-    }
-  });
+  $(window).on('load', function() {
+    $(".clients-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+      }
+    });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
 
+  });
 });
